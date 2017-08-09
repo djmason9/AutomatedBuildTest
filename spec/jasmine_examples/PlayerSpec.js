86ba281085fd1fcee6f2ab2f8a9b9f99d7b51956
Darren <djmason9@gmail.com>
2017-08-08 17:07:23 -0700

    failed test

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -2,4 +2,10 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+  
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(false);
+  });
 });
