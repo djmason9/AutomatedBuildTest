db03358e1d5fe94fa2152e568172057cec6bdc7a
Darren <djmason9@gmail.com>
2017-08-15 16:24:29 -0700

    formated

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -1,29 +1,28 @@
 describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-  
-  it("and so is a spec 2", function() {
-    a = true;
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
 
-    expect(a).toBe(true);
-  });
-  
-  it("and so is a spec 3", function() {
-    a = true;
+    it("and so is a spec 2", function() {
+        a = true;
 
-    expect(a).toBe(true);
-  });
-  
-  it("and so is a spec 4", function() {
-    a = true;
+        expect(a).toBe(true);
+    });
 
-    expect(a).toBe(true);
-  });
-  
+    it("and so is a spec 3", function() {
+        a = true;
+
+        expect(a).toBe(true);
+    });
+
+    it("and so is a spec 4", function() {
+        a = true;
+
+        expect(a).toBe(true);
+    });
+
     it("and so is a spec 5", function() {
-    a = true;
-
-    expect(a).toBe(true);
-  });
+        a = true;
+        expect(a).toBe(true);
+    });
 });
